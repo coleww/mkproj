1dbb7ae48dfd126789bb1cdd1ac15137909abba7
Cole Willsea <coleww@gmail.com>
2015-05-27 19:13:03 -0700

    readme.md

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var fs = require('fs')
-
+function toErr(err){
+  if (err) throw err;
+}
 module.exports = function(name){
   fs.mkdir(name, function(err){
     if(err) {
       console.log(err)
     } else {
-      fs.writeFile(name + '/.gitignore', '/node_modules', function (err) {
-        if (err) throw err;
-        console.log('It\'s saved!');
-      });
+      fs.writeFile(name + '/.gitignore', '/node_modules', toErr);
+      fs.writeFile(name + '/README.md', 'AndyWarhol.js\n----------------', toErr);
+
+
+
     }
 
 
--- test.js
+++ test.js
@@ -5,13 +5,18 @@ var rimraf = require('rimraf')
 var noop = function(){}
 
 tap.test('does all the stuff', function(t){
-  t.plan(2)
+  t.plan(3)
 
   mkproj('AndyWarhol.js')
 
   setTimeout(function(){
     t.ok(fs.existsSync('AndyWarhol.js'), 'mks a new directory')
-    t.equal(fs.readFileSync('AndyWarhol.js/.gitignore', {encoding: 'utf-8'}), '/node_modules')
+    t.equal(fs.readFileSync('AndyWarhol.js/.gitignore', {encoding: 'utf-8'}),
+            '/node_modules',
+            'echoes node_modules into gitignore')
+    t.equal(fs.readFileSync('AndyWarhol.js/README.md', {encoding: 'utf-8'}),
+            'AndyWarhol.js\n----------------',
+            'echoes proj name into README.md')
 
     rimraf('AndyWarhol.js', noop)
   }, 500)
