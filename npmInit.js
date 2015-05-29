c2b32731a921ed3b666c83bf5a6d369c932f0ea7
Cole Willsea <coleww@gmail.com>
2015-05-29 10:05:44 -0700

    wtf description?

--- npmInit.js
+++ npmInit.js
@@ -1,6 +1,6 @@
 module.exports = function(name){
   return "{\n  \"name\": \"" +
   name +
-  "\",\n  \"description\": \"\",\n  \"version\": \"1.0.0\",\n  \"main\": \"index.js\",\n  \"scripts\": {\n    \"build\": \"browserify index.js -o bundle.js\",\n    \"watch\": \"watchify index.js -o bundle.js --debug --verbose\"\n  },\n  \"devDependencies\": {\n    \"browserify\": \"*\",\n    \"watchify\": \"*\"\n  }\n}"
+  "\",\n  \"description\": \"Put A Description Here!\",\n  \"version\": \"1.0.0\",\n  \"main\": \"index.js\",\n  \"scripts\": {\n    \"build\": \"browserify index.js -o bundle.js\",\n    \"watch\": \"watchify index.js -o bundle.js --debug --verbose\"\n  },\n  \"devDependencies\": {\n    \"browserify\": \"*\",\n    \"watchify\": \"*\"\n  }\n}"
 }
 
