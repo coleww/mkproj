2872f5fe183a0d858c1e0619e980b9a6dd8225ee
Cole Willsea <coleww@gmail.com>
2015-07-10 16:38:06 -0700

    fix thing

--- npmInit.js
+++ npmInit.js
@@ -1,5 +1,5 @@
 module.exports = function(name){
   return "{\n  \"name\": \"" +
   name +
-  "\",\n  \"description\": \"Put A Description Here!\",\n  \"version\": \"1.0.0\",\n  \"main\": \"index.js\",\n  \"scripts\": {\n    \"build\": \"browserify www/demo.js -o www/bundle.js\",\n    \"test\": \"node test.js -o www/bundle.js\",\n    \"watch\": \"watchify www/demo.js -o www/bundle.js --debug --verbose\"\n  },\n  \"devDependencies\": {\n    \"browserify\": \"*\",\n    \"watchify\": \"*\"\n  }\n    \"tape\": \"*\",\n}"
+  "\",\n  \"description\": \"Put A Description Here!\",\n  \"version\": \"1.0.0\",\n  \"main\": \"index.js\",\n  \"scripts\": {\n    \"build\": \"browserify www/demo.js -o www/bundle.js\",\n    \"test\": \"node test.js -o www/bundle.js\",\n    \"watch\": \"watchify www/demo.js -o www/bundle.js --debug --verbose\"\n  },\n  \"devDependencies\": {\n    \"browserify\": \"*\",\n    \"tape\": \"*\",\n    \"watchify\": \"*\"\n  }\n}"
 }
