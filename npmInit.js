55e872fcd98dcecf4911da4e3aba4f6c6f1130de
Cole Willsea <coleww@gmail.com>
2015-08-02 15:54:53 -0700

    --amend

--- npmInit.js
+++ npmInit.js
@@ -4,6 +4,6 @@ module.exports = function (name) {
   '\",\n  \"description\": \"Put A Description Here!\",\n  \"version\": \"1.0.0\",\n' +
   '    \"main\": \"index.js\",\n  \"scripts\": {\n    \"build\": \"browserify www/demo.js -o www/bundle.js\",\n' +
   '    \"test\": \"standard && node test.js\",\n    \"watch\": \"watchify www/demo.js -o www/bundle.js --debug --verbose\"\n  },' +
-  '\n  \"devDependencies\": {\n    \"browserify\": \"*\",\n    \"standard\": \"*\",\n    \"tape\": \"*\",\n    ' +
+  '\n  \"devDependencies\": {\n    \"browserify\": \"*\",\n    \"standard\": \"4.5.4\",\n    \"tape\": \"4.0.1\",\n    ' +
   '\"watchify\": \"*\"\n  },\n  \"license\": \"MIT\"\n}'
 }
