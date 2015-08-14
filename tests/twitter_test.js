f0ed0e82b598436fbbed73f33dca74c4cdf5d6ba
Cole Willsea <coleww@gmail.com>
2015-08-14 08:30:22 -0700

    got dat tweety bird

--- tests/twitter_test.js
+++ tests/twitter_test.js
@@ -1,38 +1,44 @@
 var tap = require('tap')
 var mkproj = require('../')
 var fs = require('fs')
-var rimraf = require('rimraf')
-var noop = function () {}
-var exec = require('child_process').exec
 
-tap.test('makes a tweety', function (t) {
-  t.plan(20)
+var testUtils = require('./test_utils')
+var checkBasics = testUtils.checkBasics
+var checkAbsence = testUtils.checkAbsence
+var cleanUpAndRun = testUtils.cleanUpAndRun
+var checkGeneratedApp = testUtils.checkGeneratedApp
 
-  mkproj('tweety', {testing: true, twitter: true})
+var test_name = 'tweety'
 
-  console.log = (function () {
-    // var log = console.log
-    return function (msg) {
-      t.ok(msg, 'logs creation')
-      // log.call(console, msg)
-    }
-  })()
+cleanUpAndRun(test_name, testIt)
 
-  setTimeout(function () {
-    t.ok(fs.existsSync('tweety'), 'mks a new directory')
-    t.ok(fs.readFileSync('tweety/README.md', {encoding: 'utf-8'}).toString().match('tweety\n----------------'),
-            'echoes proj name into README.md')
-    t.ok(fs.existsSync('tweety/index.js'), 'mks an index.js')
-    t.ok(fs.readFileSync('tweety/tweet.js').toString().match('twit'), 'mks a twitter boilerplate file')
-    t.ok(fs.readFileSync('tweety/bot.js').toString().match('tweet'), 'mks a botfile')
-    t.ok(fs.readFileSync('tweety/package.json').toString().match('twit'),
-         'mks a package.json containing twit cuz it is tooting time')
-    t.ok(fs.existsSync('tweety/.travis.yml'), 'mks a trav')
-    t.ok(fs.existsSync('tweety/test.js'), 'mks a test file')
-    exec('cp -r tap_modules tweety/node_modules && cd tweety && standard && node test.js', function (error, stdout, stderr) {
-      t.ok(!error, 'generated module also works fail')
-      rimraf('tweety', noop)
-    })
+function testIt () {
+  tap.test('makes a tweety', function (t) {
+    t.plan(25)
 
-  }, 1000)
-})
+    mkproj('tweety', {testing: true, twitter: true})
+
+    console.log = (function () {
+      // var log = console.log
+      return function (msg) {
+        t.ok(msg, 'logs creation')
+        // log.call(console, msg)
+      }
+    })()
+
+    setTimeout(function () {
+      checkBasics(test_name, t)
+
+
+      t.ok(fs.readFileSync('tweety/tweet.js').toString().match('twit'), 'mks a twitter boilerplate file')
+      t.ok(fs.readFileSync('tweety/bot.js').toString().match('tweet'), 'mks a botfile')
+      t.ok(fs.readFileSync('tweety/package.json').toString().match('twit'),
+           'mks a package.json containing twit cuz it is tooting time')
+      t.ok(fs.existsSync('tweety/test.js'), 'mks a test file')
+
+      checkAbsence(test_name, t, ['cmd.js', 'www'])
+      checkGeneratedApp(test_name, t, 'tap')
+
+    }, 1000)
+  })
+}
