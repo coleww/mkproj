0a8fda8ef7224a01a8ea22d36758581ae7aafa2b
Cole Willsea <coleww@gmail.com>
2015-08-14 18:04:21 -0700

    dang like totall victory

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
-var tap = require('tap')
-var mkproj = require('./')
+var testIt = require('./tests/test_utils')
 
-tap.test('does nothing without a project name', function (t) {
-  t.plan(2)
-  console.log = function (msg) {
-    t.equal(msg, 'you must pass a project name!', 'warns if no proj name is passed')
-  }
-  t.equal(mkproj(), 'fail', 'process exits')
-})
+testIt('defaulty', {twitter: false, browserify: false, cli: false})
+testIt('cli', {twitter: false, browserify: false, cli: true})
+testIt('browsy', {twitter: false, browserify: true, cli: false})
+testIt('tweety', {twitter: true, browserify: false, cli: false})
+testIt('browsatweet', {twitter: true, browserify: true, cli: false})
+testIt('clibro', {twitter: false, browserify: true, cli: true})
+testIt('tweecli', {twitter: true, browserify: false, cli: true})
+testIt('everything', {twitter: true, browserify: true, cli: true})
 
-require('./tests/default_test.js')
-require('./tests/cli_test.js')
-require('./tests/everything_test.js')
-require('./tests/browserify_test.js')
-require('./tests/twitter_test.js')
+// um where to put this lol
+// tap.test('does nothing without a project name', function (t) {
+//   t.plan(2)
+//   console.log = function (msg) {
+//     t.equal(msg, 'you must pass a project name!', 'warns if no proj name is passed')
+//   }
+//   t.equal(mkproj(), 'fail', 'process exits')
+// })
--- tests/test_utils.js
+++ tests/test_utils.js
@@ -1,3 +1,5 @@
+var tap = require('tap')
+var mkproj = require('../')
 var exec = require('child_process').exec
 var rimraf = require('rimraf')
 var noop = function () {}
@@ -22,9 +24,9 @@ var checkAbsence = function (path, t, paths) {
   })
 }
 
-var cleanUpAndRun = function (test_name, cb) {
-  if (fs.existsSync(test_name)) {
-    rimraf(test_name, function () {
+var cleanUpAndRun = function (path, cb) {
+  if (fs.existsSync(path)) {
+    rimraf(path, function () {
       cb()
     })
   } else {
@@ -32,17 +34,90 @@ var cleanUpAndRun = function (test_name, cb) {
   }
 }
 
-var checkGeneratedApp = function (test_name, t, type) {
-  exec('cp -r ./' + type + '_modules ./' + test_name + '/node_modules && cd ' + test_name + ' && standard && node test.js', function (error, stdout, stderr) {
+var checkForTesty = function (path, t, type) {
+  t.ok(fs.readFileSync(path + '/package.json').toString().match('\"' + type + '\"'), 'mks a package.json containing ' + type + '.')
+  t.ok(fs.readFileSync(path + '/test.js').toString().match('\'' + type + '\''), 'mks a test file that requires ' + type + '.')
+}
+
+var checkGeneratedApp = function (path, t, type) {
+  exec('cp -r ./' + type + '_modules ./' + path + '/node_modules && cd ' + path + ' && standard && node test.js', function (error, stdout, stderr) {
     t.ok(!error, 'generated module also works' + JSON.stringify(error) + stdout + stderr)
-    rimraf(test_name, noop)
+    rimraf(path, noop)
   })
 }
 
-module.exports = {
-  checkBasics: checkBasics,
-  checkAbsence: checkAbsence,
-  cleanUpAndRun: cleanUpAndRun,
-  checkGeneratedApp: checkGeneratedApp
+var checkForCli = function (path, t) {
+  t.ok(fs.readFileSync(path + '/cmd.js').toString().match('yargs'), 'mks a CLI boilerplate file')
+  t.ok(fs.readFileSync(path + '/package.json').toString().match('yargs'), 'mks a package.json containing yargs cuz it is YARRRRRRRR time')
+}
 
+var checkForTwitter = function (path, t) {
+  t.ok(fs.readFileSync(path + '/tweet.js').toString().match('twit'), 'mks a twitter boilerplate file')
+  t.ok(fs.readFileSync(path + '/bot.js').toString().match('tweet'), 'mks a botfile')
+  t.ok(fs.readFileSync(path + '/package.json').toString().match('twit'), 'mks a package.json containing twit cuz it is tooting time')
+}
+
+var checkForBrowser = function (path, t) {
+  t.ok(fs.existsSync(path + '/www/main.css'), 'mks a main.css')
+  t.ok(fs.existsSync(path + '/www/demo.js'), 'mks a demo.js')
+  t.ok(fs.readFileSync(path + '/www/index.html').toString().match('<title>' + path + '</title>'), 'mks some html5 boilerplate')
+  var packaged = fs.readFileSync(path + '/package.json').toString()
+  t.ok(packaged.match('browserify'), 'mks a package.json containing browserify cuz it is time')
+  t.ok(packaged.match('watchify'), 'mks a package.json containing watchify cuz it is cool')
+  t.ok(packaged.match('gh-pages-deploy'), 'mks a package.json containing gh-pages-deploy cuz it is sweet')
+}
+
+module.exports = function (name, options) {
+  var count = 7 // number of files generated == number of logs that happen/get t.ok'd
+  var exclusions = [] // files to check for exclusion
+  if (options.browserify) {
+    count += 9 // 3 files generated + 6 assertions
+  } else {
+    exclusions.push('www')
+  }
+  if (options.cli) {
+    count += 3 // 1 file generated + 2 assertions
+  } else {
+    exclusions.push('cmd.js')
+  }
+  if (options.twitter) {
+    count += 5 // 2 files generated + 3 assertions
+  } else {
+    exclusions.push('tweet.js')
+    exclusions.push('bot.js')
+  }
+
+  var type = options.browserify ? 'tape' : 'tap'
+
+  cleanUpAndRun(name, reallyTestIt)
+
+  function reallyTestIt () {
+    tap.test(name, function (t) {
+      t.plan(count + exclusions.length + 12)
+      // 3 from index.js, 6 from checkBasics, 1 from generatedApp, 2 for checkForTesty
+      options.testing = true
+      mkproj(name, options)
+
+      console.log = function (msg) {
+        t.ok(msg, 'logs creation')
+      }
+
+      setTimeout(function () {
+        checkBasics(name, t)
+        checkForTesty(name, t, type)
+        if (options.browserify) {
+          checkForBrowser(name, t)
+        }
+        if (options.cli) {
+          checkForCli(name, t)
+        }
+        if (options.twitter) {
+          checkForTwitter(name, t)
+        }
+
+        checkAbsence(name, t, exclusions)
+        checkGeneratedApp(name, t, type)
+      }, 1000)
+    })
+  }
 }
