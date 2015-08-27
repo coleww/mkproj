bfa772fedd22d086ebdedbf544327525f29dbaf9
Cole Willsea <coleww@gmail.com>
2015-08-27 13:30:46 -0700

    fix the `-t failureBot` issue by setting all the args to bools

--- cmd.js
+++ cmd.js
@@ -13,22 +13,29 @@ var argv = require('yargs')
                           .example('mkproj yr-cool-twitter-bot -tweet')
                           .example('mkproj make-me-a-sandwich-please -blt')
                           .example('mkproj -b # add /www and browserify to an existing project')
+                          .boolean('b')
                           .alias('b', 'browserify')
                           .alias('b', 'browser')
                           .alias('b', 'bacon')
                           .describe('b', 'installs browserify/watchify and adds /www folder')
+                          .boolean('c')
                           .alias('c', 'cli')
                           .alias('c', 'cmd')
                           .alias('c', 'l') // -blt
                           .alias('c', 'lettuce')
                           .describe('c', 'installs yargs and adds cmd.js file')
+                          .boolean('t')
                           .alias('t', 'twitter')
                           .alias('t', 'tweet')
                           .alias('t', 'tomato')
                           .describe('t', 'installs twit and adds tweet.js file')
+                          .boolean('n')
                           .alias('n', 'noPleaseDoNotInstallThanks')
                           .describe('n', 'skip the whole "npm init/npm install/git init/initial commit" business')
                           .argv
+
+// should manually fail here if not passed a proj name OR a non n option
+// dont think yargs demand works...
 
 var projectName = argv._.join('-').replace(/\W/g, '-')
 
